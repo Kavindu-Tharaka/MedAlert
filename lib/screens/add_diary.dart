cd5a6e55eff86817714ba7fed0f10e2ed84081c8
Dilanka R.M.T <it18009132@my.sliit.lk>
2021-06-16 03:45:41 +0530

    D002 | feat: Initialize add diary

--- lib/screens/add_diary.dart
+++ lib/screens/add_diary.dart
@@ -55,23 +55,26 @@ class _AddDiaryState extends State<AddDiary> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: [
+          Image.asset(
+            'assets\\images\\MedAlert_Logo.png',
+            height: 20,
+          ),
+        ],
+      ),
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
         child: SingleChildScrollView(
           child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    size: 32,
-                    color: Colors.black26,
-                  ),
-                ),
-                SizedBox(height: 20.0),
                 Text(
                   widget.diaryFields == null
                       ? 'How do you feel today?'
@@ -89,87 +92,7 @@ class _AddDiaryState extends State<AddDiary> {
                   child: Column(
                     children: <Widget>[
                       Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: TextFormField(
-                          onEditingComplete: () {
-                            FocusScope.of(context).nextFocus();
-                          },
-                          autofocus: false,
-                          // controller: medicineNameController,
-                          maxLines: 1,
-                          keyboardType: TextInputType.number,
-                          style: TextStyle(
-                            color: Colors.black87,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(10.0),
-                            labelText: 'Sugar Level - Fasting',
-                            labelStyle: TextStyle(
-                              fontSize: 15,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(40),
-                              borderSide: BorderSide(
-                                  color: Colors.black,
-                                  style: BorderStyle.solid),
-                            ),
-                            hintText: 'mgs/dl',
-                            hintStyle: TextStyle(color: Colors.white70),
-                          ),
-                          onSaved: (input) => _diaryFieldsLevelF = input,
-                          initialValue: _diaryFieldsLevelF,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Can not be empty!';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: TextFormField(
-                          onEditingComplete: () {
-                            FocusScope.of(context).nextFocus();
-                          },
-                          autofocus: false,
-                          // controller: medicineNameController,
-                          maxLines: 1,
-                          keyboardType: TextInputType.number,
-                          style: TextStyle(
-                            color: Colors.black87,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(10.0),
-                            labelText: 'Sugar Level - Post Pradinal',
-                            labelStyle: TextStyle(
-                              fontSize: 15,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(40),
-                              borderSide: BorderSide(
-                                  color: Colors.black,
-                                  style: BorderStyle.solid),
-                            ),
-                            hintText: 'mgs/dl',
-                            hintStyle: TextStyle(color: Colors.white70),
-                          ),
-                          onSaved: (input) => _diaryFieldsLevelPP = input,
-                          initialValue: _diaryFieldsLevelPP,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Can not be empty!';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
                         child: TextFormField(
                           autofocus: false,
                           controller: _dateController,
@@ -177,14 +100,14 @@ class _AddDiaryState extends State<AddDiary> {
                           // keyboardType: TextInputType.number,
                           style: TextStyle(
                             color: Colors.black87,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20,
                           ),
                           decoration: InputDecoration(
                             contentPadding: const EdgeInsets.all(10.0),
-                            labelText: 'Report Date',
+                            labelText: 'Date',
                             labelStyle: TextStyle(
-                              fontSize: 15,
+                              fontSize: 18,
                             ),
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(40),
@@ -194,6 +117,45 @@ class _AddDiaryState extends State<AddDiary> {
                             ),
                             hintStyle: TextStyle(color: Colors.white70),
                           ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Can not be empty!';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        child: TextFormField(
+                          onEditingComplete: () {
+                            FocusScope.of(context).nextFocus();
+                          },
+                          autofocus: true,
+                          maxLines: null,
+                          keyboardType: TextInputType.text,
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20,
+                          ),
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(10.0),
+                            labelText: 'Description',
+                            labelStyle: TextStyle(
+                              fontSize: 18,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid),
+                            ),
+                            hintText: 'Description',
+                            hintStyle: TextStyle(color: Colors.white70),
+                          ),
+                          onSaved: (input) => _diaryFieldsLevelF = input,
+                          initialValue: _diaryFieldsLevelF,
                           validator: (value) {
                             if (value.isEmpty) {
                               return 'Can not be empty!';
@@ -220,8 +182,8 @@ class _AddDiaryState extends State<AddDiary> {
                                   MaterialStateProperty.all(Color(0XFF008bb0))),
                           child: Text(
                             widget.diaryFields == null
-                                ? 'Add Sugar Report'
-                                : 'Update Report',
+                                ? 'Add Entry'
+                                : 'Update Diary',
                             style:
                                 TextStyle(color: Colors.white, fontSize: 20.0),
                           ),
